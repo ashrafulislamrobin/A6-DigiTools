2719cdaaa8f8f4856c270fed7ccf2f16379db959
ashrafulislamrobin <ashrafulrobin.mail@gmail.com>
2026-03-31 00:23:46 +0600

    Fetch data from data.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { useState, useEffect } from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Stats from './components/Stats';
-
+import ProductSection from './components/ProductSection';
 import Steps from './components/Steps';
 import Pricing from './components/Pricing';
-
+import Footer from './components/Footer';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
 
   return (
     <div className="min-h-screen bg-white">
       <Navbar cartCount={cart.length} />
       <Banner />
       <Stats />
-     
+      <ProductSection
+
+      />
       <Steps />
       <Pricing />
       <Footer />
